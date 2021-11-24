09d77d7e1008f87baca8b7d39294913f8a951775
Markk86 <markmaguire1986@gmail.com>
2021-11-24 11:35:01 +0000

    First commit

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import sys
+import os
+from math import pi
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Circle:
+    def __init__(self, radius, fill='red', stroke='black'):
+        self._radius = radius
+        self.fill = fill
+        self.stroke = stroke
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+    def calculate_area(self):
+        return pi * self._radius ** 2
+
+    @property
+    def radius(self):  # Public access for_radius: read only
+        return self._radius
+
+    def __call__(self):
+        return "I am a circle"
+
+
+def main():
+    circle = Circle(5.0, fill='orange', stroke='red')
+    print(f"area = {circle.calculate_area()}")
+    print(f"area = {circle.area}")
+    print(circle())
+    circle2 = Circle(8.0)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
